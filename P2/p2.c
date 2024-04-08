d1e6965b26dd1212f00294e19ae979e0dd46592c
Rafalpv <rafaluperezvico@gmail.com>
2024-04-08 15:32:53 +0200

    Sistema de puntuacion implementado

--- P2/p2.c
+++ P2/p2.c
@@ -1,6 +1,17 @@
 #include <ncurses.h>
 #include <unistd.h>
+
 #define DELAY 30000
+
+int j1_points = 0, j2_points = 0;
+
+void puntuacion(WINDOW *window, int j_points, int y, int x){
+    
+    char points[2];
+    sprintf(points, "%d", j_points);
+    mvwprintw(window, y, x,points);
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -36,9 +47,9 @@ int main(int argc, char *argv[])
 
     int xBall = cols / 2,
         yBall = rows / 2 - 1,
-        jug1 = rows / 2 - 1, 
+        jug1 = rows / 2 - 1,
         jug2 = rows / 2 - 1;
-    
+
     int next_y, next_x;
 
     wbkgd(window, COLOR_PAIR(2));
@@ -56,14 +67,20 @@ int main(int argc, char *argv[])
 
     mvwprintw(window, rows / 2 - 1, cols / 2, "O");
 
+    mvwprintw(window, 3, 10, "0");
+    mvwprintw(window, 3, 80, "0");
+
     wrefresh(window);
 
     int direction_x = 1, direction_y = 1, ant_X, ant_Y;
 
     while (1)
     {
-        switch (getch()){
-        case 'w':;
+        //Movimiento Jugadores
+        switch (getch())
+        {
+        case 'w':
+        case 'W':
             if (jug1 - 1 > 0)
             {
                 mvwprintw(window, jug1 + 1, 1, " ");
@@ -72,6 +89,7 @@ int main(int argc, char *argv[])
             }
             break;
         case 's':
+        case 'S':
             if (jug1 + 2 < rows)
             {
                 mvwprintw(window, jug1 - 1, 1, " ");
@@ -80,6 +98,7 @@ int main(int argc, char *argv[])
             }
             break;
         case 'o':
+        case 'O':
             if (jug2 - 1 > 0)
             {
                 mvwprintw(window, jug2 + 1, cols - 2, " ");
@@ -89,6 +108,7 @@ int main(int argc, char *argv[])
 
             break;
         case 'l':
+        case 'L':
             if (jug2 + 2 < rows)
             {
                 mvwprintw(window, jug2 - 1, cols - 2, " ");
@@ -99,24 +119,48 @@ int main(int argc, char *argv[])
         }
 
         // Movimiento de la pelota
-        mvwprintw(window, ant_Y ,ant_X, " ");
-        mvwprintw(window, yBall, xBall, "O");
-        usleep(DELAY);
-
+        
         next_x = xBall + direction_x;
         next_y = yBall + direction_y;
-        ant_X = xBall; ant_Y = yBall;
+        ant_X = xBall;
+        ant_Y = yBall;
 
-        if(next_x >= cols || next_x < 0)
+        if (next_x > cols || next_x < -1)
             direction_x *= -1;
         else
             xBall += direction_x;
 
-        if(next_y >= rows || next_y < 0)
+        if (next_y >= rows || next_y < 0)
             direction_y *= -1;
         else
             yBall += direction_y;
-    
+
+        //Puntuacion
+        if(xBall == -1){
+            j2_points++;
+            puntuacion(window,j2_points, 3, 80);
+            xBall = cols / 2; yBall = rows / 2 - 1;
+            direction_x*=-1;
+            usleep(5000000);
+        }
+        
+        if(xBall == cols){
+            j1_points++;
+            puntuacion(window,j1_points, 3, 10);
+            xBall = cols / 2; yBall = rows / 2 - 1;
+            direction_x*=-1;
+            usleep(5000000);
+        }
+        
+        // Colision con los jugadores
+
+
+        // Pintamos la pelota
+        mvwprintw(window, ant_Y, ant_X, " ");
+        mvwprintw(window, yBall, xBall, "O");
+        usleep(DELAY);
+
+
         wrefresh(window);
     }
 
