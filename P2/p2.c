08f107dbfd2d5e9073c74b302e7f19381f632a23
Rafalpv <rafaluperezvico@gmail.com>
2024-04-09 12:44:39 +0200

    Cambios en de Diseño. Pagina Final

--- P2/p2.c
+++ P2/p2.c
@@ -4,58 +4,43 @@
 #define DELAY 40000
 
 int j1_points = 0, j2_points = 0;
+int rows, cols;
 
-void puntuacion(WINDOW *window, int j_points, int y, int x)
+WINDOW *pantallaInicial()
 {
-
-    char points[2];
-    sprintf(points, "%d", j_points);
-    mvwprintw(window, y, x, points);
-}
-
-int main(int argc, char *argv[])
-{
-
-    int rows, cols;
-
-    // Pantalla Inicial
-
     initscr();
     curs_set(FALSE);
 
     start_color();
     noecho();
-    init_pair(1, COLOR_WHITE, COLOR_BLUE);
-    init_pair(2, COLOR_BLACK, COLOR_GREEN);
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
 
     refresh();
 
     getmaxyx(stdscr, rows, cols);
     WINDOW *window = newwin(rows, cols, 0, 0);
-    wbkgd(window, COLOR_PAIR(1));
     box(window, '|', '-');
 
-    mvwprintw(window, 6, 13, "Jugador 1: Izquierda -> W(Arriba) S(Abajo)\n");
-    mvwprintw(window, 7, 13, "Jugador 2: Derecha -> O(Arriba) L(Abajo)");
-    mvwprintw(window, 10, 3, "PULSA UNA TECLA PARA INICIAR!!. GANA EL PRIMERO QUE LLEGUE A 5 PUNTOS.");
+    mvprintw(rows / 2 - rows / 4, 0, "\t           oooooooooo                                        \n"
+                                     "\t           888    888  ooooooo    ooooooo    oooooooo8       \n"
+                                     "\t           888oooo88 888     888 888   888  888    88o       \n"
+                                     "\t           888       888     888 888   888   888oo888o       \n"
+                                     "\t          o888o        88ooo88  o888o o888o 888     888      \n"
+                                     "\t                                             888ooo888     \n\n"
+                                     "\t \t\t JUGADOR 1 your controls are 'w' and 's' \n"
+                                     "\t \t\t JUGADOR 2 your controls are 'o' and 'l' \n"
+                                     "\t \t\t Pulsa cualquiera tecla");
+
     wrefresh(window);
-    getch();
 
-    werase(window);
+    return window;
+}
 
-    //  Comienzo del Juego
-
-    int xBall = cols / 2,
-        yBall = rows / 2 - 1,
-        jug1 = rows / 2 - 1,
-        jug2 = rows / 2 - 1;
-
-    int next_y, next_x;
-
-    wbkgd(window, COLOR_PAIR(2));
+void estadoInicial(WINDOW *window)
+{
+    wbkgd(window, COLOR_PAIR(1));
     nodelay(stdscr, TRUE);
     keypad(stdscr, TRUE);
-    cbreak();
 
     mvwprintw(window, rows / 2 - 2, 1, "|");
     mvwprintw(window, rows / 2 - 1, 1, "|");
@@ -65,17 +50,64 @@ int main(int argc, char *argv[])
     mvwprintw(window, rows / 2 - 1, cols - 2, "|");
     mvwprintw(window, rows / 2, cols - 2, "|");
 
-    mvwprintw(window, rows / 2 - 1, cols / 2, "O");
+    mvwprintw(window, rows / 2 - 1, cols / 2, "o");
 
-    mvwprintw(window, 3, 10, "0");
-    mvwprintw(window, 3, 80, "0");
+    mvwprintw(window, 3, 40, "0");
+    mvwprintw(window, 3, 60, "0");
 
     wrefresh(window);
+}
+
+void puntuacion(WINDOW *window, int j_points, int y, int x)
+{
+    mvwprintw(window, y, x, "%d", j_points);
+}
+
+void finPartida(WINDOW *window)
+{
+    werase(window);
+    nodelay(stdscr, false);
+
+    box(window, '|', '-');
+
+    mvprintw(5, 5, "JUGADOR 1 : %d", j1_points);
+
+    mvprintw(8, 5, "JUGADOR 2 : %d", j2_points);
+
+    wrefresh(window);
+
+    getch();
+}
+
+int main(int argc, char *argv[])
+{
+
+
+    WINDOW *window = pantallaInicial();
+    keypad(stdscr, true);
+
+    getch();
+
+    werase(window);
+
+///////////////////////////////////////////////
+
+    int xBall = cols / 2,
+        yBall = rows / 2 - 1,
+        jug1 = rows / 2 - 1,
+        jug2 = rows / 2 - 1,
+        next_y = -1, 
+        next_x = -1;
+
+    estadoInicial(window);
 
     int direction_x = 1, direction_y = 1, ant_X, ant_Y;
 
     while (1)
     {
+        mvvline(0, cols / 2, ACS_VLINE, cols);
+
+
         // Movimiento Jugadores
         switch (getch())
         {
@@ -116,10 +148,20 @@ int main(int argc, char *argv[])
                 jug2++;
             }
             break;
+        case 'p':
+        case 'P':
+            getchar();
+            break;
         }
 
-        // Movimiento de la pelota
+        // Colision con los jugadores
+        if (xBall == 2 && (yBall >= jug1 - 1 && yBall <= jug1 + 1))
+            direction_x *= -1;
+        else if (xBall == cols - 3 && (yBall >= jug2 - 1 && yBall <= jug2 + 1))
+            direction_x *= -1;
 
+
+        // Movimiento de la pelota
         next_x = xBall + direction_x;
         next_y = yBall + direction_y;
         ant_X = xBall;
@@ -130,44 +172,39 @@ int main(int argc, char *argv[])
         else
             xBall += direction_x;
 
-        // if (next_y >= rows || next_y < 0)
-        //     direction_y *= -1;
-        // else
-        //     yBall += direction_y;
+        if (next_y >= rows || next_y < 0)
+            direction_y *= -1;
+        else
+            yBall += direction_y;
 
         // Puntuacion
         if (xBall == -1)
         {
             j2_points++;
-            puntuacion(window, j2_points, 3, 80);
+            mvwprintw(window, 3, 60, "%d", j2_points);
             xBall = cols / 2;
             yBall = rows / 2 - 1;
             direction_x *= -1;
-            usleep(1000000);
+            usleep(10000);
         }
 
         if (xBall == cols)
         {
             j1_points++;
-            puntuacion(window, j1_points, 3, 10);
+            mvwprintw(window, 3, 40, "%d", j1_points);
             xBall = cols / 2;
             yBall = rows / 2 - 1;
             direction_x *= -1;
-            usleep(1000000);
+            usleep(10000);
         }
-
-        // Colision con los jugadores
-
-        if(xBall == 2 && (yBall >= jug1-1 && yBall <= jug1+1))
-            direction_x *= -1;
-        else if(xBall == cols-3 && (yBall >= jug2-1 && yBall <= jug2+1))
-            direction_x *= -1;
-        
 
         // Pintamos la pelota
         mvwprintw(window, ant_Y, ant_X, " ");
-        mvwprintw(window, yBall, xBall, "O");
+        mvwprintw(window, yBall, xBall, "o");
         usleep(DELAY);
+
+        if (j1_points == 3 || j2_points == 3)
+            finPartida(window);
 
         wrefresh(window);
     }
